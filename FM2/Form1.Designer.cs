c5f21f999affb9e8df0ef0df3c12113dcb60d182
Jason <Jason@JASON-PC>
2016-06-20 09:07:08 -0400

    jjjk

--- FM2/Form1.Designer.cs
+++ FM2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FM2
 {
-    partial class Form1
+    partial class fmPlayerEvalForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importCsvToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripFilePath = new System.Windows.Forms.ToolStripStatusLabel();
+            this.playerGrid = new System.Windows.Forms.DataGridView();
+            this.mainMenuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.playerGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(875, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.importCsvToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // importCsvToolStripMenuItem
+            // 
+            this.importCsvToolStripMenuItem.Name = "importCsvToolStripMenuItem";
+            this.importCsvToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.importCsvToolStripMenuItem.Text = "Import Csv";
+            this.importCsvToolStripMenuItem.Click += new System.EventHandler(this.importCsvToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripFilePath});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 488);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(875, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripFilePath
+            // 
+            this.toolStripFilePath.Name = "toolStripFilePath";
+            this.toolStripFilePath.Size = new System.Drawing.Size(77, 17);
+            this.toolStripFilePath.Text = "Choose a File";
+            // 
+            // playerGrid
+            // 
+            this.playerGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.playerGrid.Location = new System.Drawing.Point(12, 36);
+            this.playerGrid.Name = "playerGrid";
+            this.playerGrid.Size = new System.Drawing.Size(535, 348);
+            this.playerGrid.TabIndex = 2;
+            // 
+            // fmPlayerEvalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(875, 510);
+            this.Controls.Add(this.playerGrid);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "fmPlayerEvalForm";
+            this.Text = "FM Player Evaluator";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.playerGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importCsvToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripFilePath;
+        private System.Windows.Forms.DataGridView playerGrid;
     }
 }
 
--- FM2/Form1.cs
+++ FM2/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +12,94 @@ using System.Windows.Forms;
 
 namespace FM2
 {
-    public partial class Form1 : Form
+    public partial class fmPlayerEvalForm : Form
     {
-        public Form1()
+        OpenFileDialog ofd = new OpenFileDialog();
+        public fmPlayerEvalForm()
         {
             InitializeComponent();
         }
+        public void readCsv(string fileName)
+        {
+            DataTable dt = new DataTable();
+
+            using (OleDbConnection cn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"" + Path.GetDirectoryName(fileName) + "\";Extended Properties='text;HDR=yes;FMT=Delimited(,)';"))
+            {
+                using (OleDbCommand cmd = new OleDbCommand(string.Format("select * from [{0}]", new FileInfo(fileName).Name), cn))
+                {
+                    cn.Open();
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+
+                        var allLines = File.ReadAllLines(fileName);
+                        var query = from line in allLines
+                                    let data = line.Split(',')
+                                    select new
+                                    {
+                                        PlayerName = data[0],
+                                        Aggression = Int32.Parse(data[1]),
+                                        WorkRate = Int32.Parse(data[2])
+                                    };
+                        //int Count = 0;
+
+                       /* List<Player> players = new List<Player>();
+
+                        foreach (var s in query)
+                        {
+                            //Count++;
+                            Player newPlayer = new Player();
+                            newPlayer.PlayerName = s.PlayerName;
+                            newPlayer.Aggression = s.Aggression;
+                            newPlayer.WorkRate = s.WorkRate;
+                            players.Add(newPlayer);
+
+                        }*/
+
+                       /* playerContext = new fmEvalModel();
+
+                        try
+                        {
+                            var playerQuery = from d in playerContext.Players
+                                              orderby d.Id
+                                              select d;
+
+                            //this.departmentList.DisplayMember = "Name";
+                            dataGridView.DataSource = ((ObjectQuery)playerQuery).Execute(MergeOption.AppendOnly);
+                            //playersDataSet.Lo
+                            //schoolContext.SaveChanges();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        */
+
+
+                    }
+                }
+            }
+        }
+
+        private void importCsvToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "CSV|*.csv";
+            // original before I found out about ofd.Filter
+            //string pattern = ".*\\.csv$";
+            //if (ofd.ShowDialog() == DialogResult.OK && Regex.IsMatch(ofd.FileName.Trim(), pattern, RegexOptions.IgnoreCase))
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //Console.Write(pattern);
+                //Console.Write(ofd);
+                toolStripFilePath.Text = ofd.FileName;
+                //dataGridView.DataSource = readCsv(ofd.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Please select a CSV only", "FM Eval", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+
+        }
+
     }
 }
